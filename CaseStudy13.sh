0018a91f4a56f65a2ccbb03d3907c116bcbbf2cc
Maulikchhabra <46771434+Maulikchhabra@users.noreply.github.com>
2020-01-20 22:14:32 +0530

    scenario 13

---
+++ CaseStudy13.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+read -p "enter the password" password
+read -p "enter file to be encrypted:" fname
+name=$(find /home/maulik/ -name $fname)
+echo "$fname"
+if [ -f "$name" ]
+then
+ if [ ${name: -4} == ".gpg" ]
+  then 
+    filename="${name%.*}"
+    result=$(gpg -d --batch --passphrase $password $name)
+    echo "$result">>$filename
+    rm $name
+  fi
+fi
+if [ -d "$name" ]
+then
+ for f in "$name"/*;
+ do
+  if [ ${f: -4} == ".gpg" ]
+  then
+   filename="${f%.*}"
+   result=$(gpg -d --batch --passphrase $password $f)
+   echo "$result">>filename
+   rm $f
+  fi
+ done
+fi 
